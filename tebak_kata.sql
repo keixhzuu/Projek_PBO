54606517d08de1d551685c9337382c088e766990
Melania <melania.ints18@gmail.com>
2025-05-10 14:41:24 +0700

    Add files via upload

---
+++ tebak_kata.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 10, 2025 at 09:41 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tebak_kata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_kata`
+--
+
+CREATE TABLE `daftar_kata` (
+  `id` int(11) NOT NULL,
+  `kata` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `daftar_kata`
+--
+
+INSERT INTO `daftar_kata` (`id`, `kata`) VALUES
+(1, 'jaemin'),
+(2, 'renjun'),
+(4, 'mark'),
+(5, 'yunho'),
+(6, 'wooyoung');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `daftar_kata`
+--
+ALTER TABLE `daftar_kata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `daftar_kata`
+--
+ALTER TABLE `daftar_kata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
